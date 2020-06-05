50cbd1f15b9a17fce6147de8476dd85fa46a99dd
ntds12 <ntnirvana91@gmail.com>
2020-06-05 09:03:04 -0400

    lastC1

--- src/components/Navbar/Menu/menu.tsx
+++ src/components/Navbar/Menu/menu.tsx
@@ -14,7 +14,7 @@ const menu:React.FC<any> = ({close,changeCloseToTrue}) => {
       style={{
         "overflow":"hidden",
         "width":"100%",
-        "transition":"all .5s linear",
+        "transition":"transform .5s ease",
         "willChange":"transform",
         "transform":`translateX(${close?"-100vw":"0px"}) translateZ(0)`
       }}
