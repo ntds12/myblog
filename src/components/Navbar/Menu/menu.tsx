e2240dc3dc21340e145e96f3c37190280a41ea67
ntds12 <ntnirvana91@gmail.com>
2020-06-21 14:59:05 -0500

    lastcommit21jun

--- src/components/Navbar/Menu/menu.tsx
+++ src/components/Navbar/Menu/menu.tsx
@@ -1,42 +1,47 @@
-import React,{
-  useState, useEffect, useLayoutEffect
-} from 'react';
+import React, { useState, useEffect, useLayoutEffect } from "react"
 
-import * as MenuStyles from './Menu.module.scss';
-import { Link } from 'gatsby';
+import * as MenuStyles from "./Menu.module.scss"
+import { Link } from "gatsby"
 
-const menu:React.FC<any> = ({close,changeCloseToTrue}) => {
-  
-  
+const menu: React.FC<any> = ({ close, changeCloseToTrue }) => {
   return (
-    <div 
+    <div
       className={MenuStyles.menu}
       style={{
-        "overflow":"hidden",
-        "width":"100%",
-        "transition":"transform .3s ease",
-        WebkitTransition:"transform .3s ease",
-        "willChange":"transform",
-        "transform":`translateX(${close?"-100vw":"0px"}) translateZ(0)`,
-        WebkitTransform:`translateX(${close?"-100vw":"0px"}) translateZ(0)`,
+        overflow: "hidden",
+        width: "100%",
+        willChange: "transform",
+        transform: "translateX(-100vw)",
+        animation: close
+          ? "closeMenu .3s ease forwards"
+          : "openMenu .3s ease forwards",
       }}
     >
       <div className={MenuStyles.contents}>
-        <p className={MenuStyles.p}>&nbsp;&nbsp;table_of_contents
-        
-        <i className={MenuStyles.icon} onClick={()=>{
-          changeCloseToTrue();
-        }}><b>&nbsp;&nbsp;&nbsp;&nbsp;x</b></i>
+        <p className={MenuStyles.p}>
+          &nbsp;&nbsp;table_of_contents
+          <i
+            className={MenuStyles.icon}
+            onClick={() => {
+              changeCloseToTrue()
+            }}
+          >
+            <b>&nbsp;&nbsp;&nbsp;&nbsp;x</b>
+          </i>
         </p>
         <p className="p_blog">
-          <Link to="/blog" className={MenuStyles.p_blog_link} onClick={changeCloseToTrue}>
+          <Link
+            to="/blog"
+            className={MenuStyles.p_blog_link}
+            onClick={changeCloseToTrue}
+          >
             Blog
           </Link>
         </p>
-        <hr className={MenuStyles.p_blog_link_hr}/>
+        <hr className={MenuStyles.p_blog_link_hr} />
       </div>
     </div>
   )
 }
 
-export default menu;
+export default menu
